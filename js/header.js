ea7e80b6de48a34f67407db38c53097cdf7d0fa7
Enrique larez <worldmotion.venezuela@gmail.com>
2017-04-11 11:10:42 -0400

    inicio delproyecto

---
+++ js/header.js
@@ -0,0 +1,98 @@
+// /*$(document).ready(function(){
+// 	$("header").css({"height":$(window).height() + "px"});
+// 	var flag = false;
+// 	var scroll;
+
+// 	$(window).scroll(function(){
+// 		scroll = $(window).scrollTop();
+// 		if (scroll > 200) {
+// 			if (!flag) {
+// 			$("nav").css({"background-color":"#3c3c3c"});
+
+// 			};
+// 			flag = true;
+// 		}
+// 		else{
+// 			if (flag) {
+// 			$$("nav").css({"background-color":"transparent"});
+// 			flag = false;
+// 			};
+// 		}
+
+// 	});
+
+
+// });*/
+
+// $(document).ready(function(){
+
+// 	$("header").css({"height":$(window).height() + "px"});
+
+// 	var flag = false;
+// 	var scroll;
+
+// 	$(window).scroll(function(){
+// 		scroll = $(window).scrollTop();
+
+// 		if(scroll > 200){
+// 			if(!flag){
+				
+
+// 				$("nav").css({"background-color": "rgba(56, 48, 72, 0.9)"});
+// 				$(".cambiar").css({"color": "#fff"});
+// 				$(".cambiar").hover({"color": "blue"});
+// 				flag = true;
+// 			}
+// 		}else{
+// 			if(flag){
+				
+
+// 				$("nav").css({"background-color": "rgba(56, 48, 72, 0.7)"});
+// 				$(".cambiar").css({"color": "#4d4d4d"});
+
+// 				flag = false;
+// 			}
+// 		}
+
+
+// 	});
+
+// 	$(document).ready(main);
+
+// var contador = 1;
+
+// function main () {
+// 	$('.menu_bar').click(function(){
+// 		if (contador == 1) {
+// 			$('nav').animate({
+// 				left: '0'
+// 			});
+// 			contador = 0;
+// 		} else {
+// 			contador = 1;
+// 			$('nav').animate({
+// 				left: '-100%'
+// 			});
+// 		}
+// 	});
+
+// 	// Mostramos y ocultamos submenus
+// 	$('.submenu').click(function(){
+// 		$(this).children('.children').slideToggle();
+// 	});
+// }
+
+
+ 
+
+// 	var x = 0;
+
+//  	$("imput").keypress(function(){
+//  		$("span").text(x += 1);
+//  	});
+
+// });
+
+$(document).ready(function(){
+	alert("Hola");
+}
